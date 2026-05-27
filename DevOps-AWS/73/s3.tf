49866616667598fa88793014614115d1477a64b3
Laraib Ahmad Siddiqui <laraibks@gmail.com>
2026-05-28 00:00:49 +0530

    Laraib

--- DevOps-AWS/73/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "tf-s3-20250510" {
-  bucket = "tf-s3-20250510"
-
-  tags = {
-    Name        = "tf-s3-20250510"
-    Environment = "dev"
-  }
-}
--- DevOps-AWS/73/variable.tf
+++
@@ -1,5 +0,0 @@
-variable "tf-vpc_cidr_block" {
-    description = "CIDR block for the VPC"
-    default = "10.0.0.20/10"
-    type = string
-}
